2a6a436cfa0664d5efeaea6cde41952bffcacd73
igorevcarvalho <33268458+igorevcarvalho@users.noreply.github.com>
2018-03-20 19:56:00 +0000

    Add files via upload

---
+++ js/sw.js
@@ -0,0 +1,59 @@
+var staticCacheName = 'mws-restaurant-static-v1';
+
+var allCaches = [
+  staticCacheName
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(staticCacheName).then(function(cache) {
+      return cache.addAll([
+        '/',
+        //'../index.html',
+        //'../restaurant.html',
+        '../css/styles.css',
+        'dbhelper.js',
+        //'main.js',
+        //'restaurant_info.js',
+        '../img/1.jpg',
+        '../img/2.jpg',
+        '../img/3.jpg',
+        '../img/4.jpg',
+        '../img/5.jpg',
+        '../img/6.jpg',
+        '../img/7.jpg',
+        '../img/8.jpg',
+        '../img/9.jpg',
+        '../img/10.jpg',
+        '../data/restaurants.json'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function(cacheName) {
+          return cacheName.startsWith('mws-restaurant-') &&
+                 !allCaches.includes(cacheName);
+        }).map(function(cacheName) {
+          return caches.delete(cacheName);
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener('message', function(event) {
+  if (event.data.action === 'skipWaiting') self.skipWaiting();    
+});
